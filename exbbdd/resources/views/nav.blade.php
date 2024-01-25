757e51a693942647106620a004db02e51efe1e29
PabloRomanCalero <pabloromcal@mdolivar.com>
2024-01-25 18:29:34 +0100

    Segundo commit

---
+++ exbbdd/resources/views/nav.blade.php
@@ -0,0 +1,7 @@
+<nav class="nav">
+  <a class="nav-link active" aria-current="page" href="{{ route('/') }}">Nuevo libro</a>
+  <a class="nav-link" >Listado libro</a>
+  <a class="nav-link" >Listado Socios</a>
+  <a class="nav-link" >Prestamo de libros</a>
+  <a class="nav-link" >Filtro de libros</a>
+</nav>
---
+++ exbbdd/resources/views/nuevo_libro.blade.php
@@ -0,0 +1,9 @@
+@extends('plantilla')
+@section('titulo', 'Libro nuevo')
+@section('contenido')
+    <h1>Nuevo libro</h1>
+    <form>
+        <p>Titulo:</p>
+        <input type="text">
+    </form>
+@endsection
